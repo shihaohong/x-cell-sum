30797d13e6d7de77e7adafd38293ce0df1df9ebb
Shi-Hao Hong <shihaohong94@gmail.com>
2017-04-28 04:10:11 -0700

    implemented render data rows

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -75,7 +75,7 @@ class TableModel {
 module.exports = TableModel;
 },{}],5:[function(require,module,exports){
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTR, createTD, createTH } = require('./dom-util');
 
 class TableView {
 	constructor(model){
@@ -89,11 +89,12 @@ class TableView {
 
 	initDomReferences() {
 		this.headerRowEl = document.querySelector('THEAD TR');
-
+		this.sheetBodyEl = document.querySelector('TBODY');
 	}
 
 	renderTable() {
 		this.renderTableHeader();
+		this.renderTableBody();
 	}
 
 	renderTableHeader() {
@@ -103,6 +104,22 @@ class TableView {
 		getLetterRange('A', this.model.numCols)
 			.map(colLabel => createTH(colLabel))
 			.forEach(th => this.headerRowEl.appendChild(th));
+	}
+
+	renderTableBody() {
+		const fragment = document.createDocumentFragment();
+		for (let row = 0; row < this.model.numRows; row++){
+			const tr = createTR();
+			for (let col = 0; col < this.model.numCols; col++){
+				const position = {col: col, row: row}; // generates the position of each box
+				const value = this.model.getValue(position); // gets the value thats in that position
+				const td = createTD(value); // creates a table item with that given value
+				tr.appendChild(td); // appends a column element to the table row 
+			}
+			fragment.appendChild(tr); // appends the entire row to the fragment
+		}
+		removeChildren(this.sheetBodyEl);
+		this.sheetBodyEl.appendChild(fragment);
 	}
 }
 
