bfce514921b643cdd9427fdcfc5187db0c3e3e9b
Shi-Hao Hong <shihaohong94@gmail.com>
2017-04-28 03:31:26 -0700

    implemented column headers

---
+++ client/js/table-view.js
@@ -0,0 +1,33 @@
+const { getLetterRange } = require('./array-util');
+const { removeChildren, createTH } = require('./dom-util');
+
+class TableView {
+	constructor(model){
+		this.model = model
+	}
+
+	init() {
+		this.initDomReferences();
+		this.renderTable();
+	}
+
+	initDomReferences() {
+		this.headerRowEl = document.querySelector('THEAD TR');
+
+	}
+
+	renderTable() {
+		this.renderTableHeader();
+	}
+
+	renderTableHeader() {
+		// clear header row
+		removeChildren(this.headerRowEl);
+		// get letters and build elements
+		getLetterRange('A', this.model.numCols)
+			.map(colLabel => createTH(colLabel))
+			.forEach(th => this.headerRowEl.appendChild(th));
+	}
+}
+
+module.exports = TableView;
