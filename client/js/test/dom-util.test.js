16d43385f6012b3fbf704de0c7d6d9bb0d42eb6f
Shi-Hao Hong <shihaohong94@gmail.com>
2017-04-28 02:52:02 -0700

    generated table-model.js

--- client/js/test/dom-util.test.js
+++ client/js/test/dom-util.test.js
@@ -1,7 +1,40 @@
-const { removeChildren } = require ('../dom-util');
+const { createTR,
+		createTD,
+		createTH,
+		removeChildren } = require ('../dom-util');
 
 describe('dom-util',  () => {
-	describe('removeChildre', () => {
+	
+	describe('DOM creation functions', () => {
+		describe('createTH', () => {
+			it('produces valid TH element', () => {
+				const el = createTH();
+				expect(el.tagName).toBe('TH');
+			});
+
+			it('sets the text of the TH', () => {
+				const text = 'Oh that\'s just great! Well, game over, man!';
+				const el = createTH(text);
+				expect(el.textContent).toEqual(text);
+			});
+		});
+
+		describe('createTD', () => {
+			it('produces valid TD element', () => {
+				const el = createTD();
+				expect(el.tagName).toBe('TD');
+			});
+		});
+
+		describe('createTR', () => {
+			it('produces valid TR element', () => {
+				const el = createTR();
+				expect(el.tagName).toBe('TR');
+			});
+		});
+	});
+
+	describe('removeChildren', () => {
 		it('removes one child', () => {
 			// sets up initial state
 			const parent = document.createElement('DIV');
